07e3796aae78266c47047bad6cf762d0f684f900
sigmade <sychyov1991@mail.ru>
2020-07-02 22:40:30 +0300

    Add project files.

---
+++ MVCnAPI/Data/MVCnAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCnAPI.Models;
+
+namespace MVCnAPI.Data
+{
+    public class MVCnAPIContext : DbContext
+    {
+        public MVCnAPIContext (DbContextOptions<MVCnAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCnAPI.Models.Person> Person { get; set; }
+    }
+}
